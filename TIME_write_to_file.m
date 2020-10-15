ecab86058fe71b8b8bbf41abbc81eda7f854cd5e
wardvp <63638856+wardvp@users.noreply.github.com>
2020-10-15 12:05:51 +0200

    save output times to runinfo.mat, new structure for output variable info

--- TIME_write_to_file.m
+++ TIME_write_to_file.m
@@ -7,65 +7,70 @@ function [io,OUTFILE] = TIME_write_to_file(OUTFILE,io,OUT,grid,t,time,C)
 %%% Specify variables to be written
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if t==1
-    io.varsout = [];
-    io.unitsout = [];
-    io.descout = [];
-    io.typeout = [];
-    if io.out_surface
-        io.varsout{end+1} = 'cmb';              io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Climatic mass balance';
-        io.varsout{end+1} = 'Tsurf';            io.unitsout{end+1} = 'K';               io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Surface temperature';
-        io.varsout{end+1} = 'climT';            io.unitsout{end+1} = 'K';               io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Air temperature';
-        io.varsout{end+1} = 'climP';            io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Precipitation';
-        io.varsout{end+1} = 'climC';            io.unitsout{end+1} = 'fraction';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Cloud cover';
-        io.varsout{end+1} = 'climRH';           io.unitsout{end+1} = 'fraction';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Relative humidity';
-        io.varsout{end+1} = 'climWS';           io.unitsout{end+1} = 'm s-1';           io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Wind speed';
-        io.varsout{end+1} = 'climPres';         io.unitsout{end+1} = 'Pa';              io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Air pressure';
-        io.varsout{end+1} = 'climrain';         io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Rainfall';
-        io.varsout{end+1} = 'climsnow';         io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Snowfall';
-        io.varsout{end+1} = 'snowmass';         io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Snow mass';
-        io.varsout{end+1} = 'cmb_cumulative';   io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Cumulative mass balance';
-        io.varsout{end+1} = 'melt';             io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Melt';
-        io.varsout{end+1} = 'refr';             io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Refreezing';
-        io.varsout{end+1} = 'runoff';           io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Runoff';
-        io.varsout{end+1} = 'runoff_surf';      io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Surface runoff';
-        io.varsout{end+1} = 'runoff_slush';     io.unitsout{end+1} = 'm w.e.';          io.typeout{end+1} = 'sum';      io.descout{end+1} = 'Slush runoff';
-        io.varsout{end+1} = 'SWin';             io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Incoming SW radiation';
-        io.varsout{end+1} = 'SWout';            io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Reflected SW radiation';
-        io.varsout{end+1} = 'LWin';             io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Incoming LW radiation';
-        io.varsout{end+1} = 'LWout';            io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Outgoing LW radiation';
-        io.varsout{end+1} = 'SHF';              io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Sensible heat flux';
-        io.varsout{end+1} = 'LHF';              io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Latent heat flux';
-        io.varsout{end+1} = 'GHF';              io.unitsout{end+1} = 'W m^{-2}';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Subsurface heat flux';
-        io.varsout{end+1} = 'surfH';            io.unitsout{end+1} = 'm';               io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Surface height';
-        io.varsout{end+1} = 'alb';              io.unitsout{end+1} = 'fraction';        io.typeout{end+1} = 'mean';     io.descout{end+1} = 'Albedo';
-    end
-    if io.out_subsurface
-        io.varsout{end+1} = 'subD';             io.unitsout{end+1} = 'kg m^{-3}';       io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Density';
-        io.varsout{end+1} = 'subT';             io.unitsout{end+1} = 'K';               io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Temperature';         
-        io.varsout{end+1} = 'subS';             io.unitsout{end+1} = 'mm w.e.';         io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Slush water content'; 
-        io.varsout{end+1} = 'subW';             io.unitsout{end+1} = 'mm w.e.';         io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Irreducible water';   
-        io.varsout{end+1} = 'subZ';             io.unitsout{end+1} = 'm';               io.typeout{end+1} = 'sample';   io.descout{end+1} = 'Layer thickness';
-    end
+    io.output_times = [];
+    OUTFILE.varsout = cell(1,4);
+    OUTFILE.varsout(1,:)     = {'cmb'           ;'m w.e.'   ;'sum'      ;'Climatic mass balance'};
+    OUTFILE.varsout(end+1,:) = {'Tsurf'         ;'K'        ;'mean'     ;'Surface temperature'};
+    OUTFILE.varsout(end+1,:) = {'climT'         ;'K'        ;'mean'     ;'Air temperature'};
+    OUTFILE.varsout(end+1,:) = {'climP'         ;'m w.e.'   ;'sum'      ;'Precipitation'};
+    OUTFILE.varsout(end+1,:) = {'climC'         ;'fraction' ;'mean'     ;'Cloud cover'};
+    OUTFILE.varsout(end+1,:) = {'climRH'        ;'fraction' ;'mean'     ;'Relative humidity'};
+    OUTFILE.varsout(end+1,:) = {'climWS'        ;'m s-1'    ;'mean'     ;'Wind speed'};
+    OUTFILE.varsout(end+1,:) = {'climPres'      ;'Pa'       ;'mean'     ;'Air pressure'};
+    OUTFILE.varsout(end+1,:) = {'climrain'      ;'m w.e.'   ;'sum'      ;'Rainfall'};
+    OUTFILE.varsout(end+1,:) = {'climsnow'      ;'m w.e.'   ;'sum'      ;'Snowfall'};
+    OUTFILE.varsout(end+1,:) = {'snowmass'      ;'m w.e.'   ;'mean'     ;'Snow mass'};
+    OUTFILE.varsout(end+1,:) = {'cmb_cumulative';'m w.e.'   ;'mean'     ;'Cumulative mass balance'};
+    OUTFILE.varsout(end+1,:) = {'melt'          ;'m w.e.'   ;'sum'      ;'Melt'};
+    OUTFILE.varsout(end+1,:) = {'refr'          ;'m w.e.'   ;'sum'      ;'Refreezing'};
+    OUTFILE.varsout(end+1,:) = {'runoff'        ;'m w.e.'   ;'sum'      ;'Runoff'};
+    OUTFILE.varsout(end+1,:) = {'runoff_surf'   ;'m w.e.'   ;'sum'      ;'Surface runoff'};
+    OUTFILE.varsout(end+1,:) = {'runoff_slush'  ;'m w.e.'   ;'sum'      ;'Slush runoff'};
+    OUTFILE.varsout(end+1,:) = {'SWin'          ;'W m^{-2}' ;'mean'     ;'Incoming SW radiation'};
+    OUTFILE.varsout(end+1,:) = {'SWout'         ;'W m^{-2}' ;'mean'     ;'Reflected SW radiation'};
+    OUTFILE.varsout(end+1,:) = {'LWin'          ;'W m^{-2}' ;'mean'     ;'Incoming LW radiation'};
+    OUTFILE.varsout(end+1,:) = {'LWout'         ;'W m^{-2}' ;'mean'     ;'Outgoing LW radiation'};
+    OUTFILE.varsout(end+1,:) = {'SHF'           ;'W m^{-2}' ;'mean'     ;'Sensible heat flux'};
+    OUTFILE.varsout(end+1,:) = {'LHF'           ;'W m^{-2}' ;'mean'     ;'Latent heat flux'};
+    OUTFILE.varsout(end+1,:) = {'GHF'           ;'W m^{-2}' ;'mean'     ;'Subsurface heat flux'};
+    OUTFILE.varsout(end+1,:) = {'surfH'         ;'m'        ;'sample'   ;'Surface height'};
+    OUTFILE.varsout(end+1,:) = {'alb'           ;'fraction' ;'mean'     ;'Albedo'};
+    OUTFILE.varsout(end+1,:) = {'shade'         ;'fraction' ;'mean'     ;'Shading (0=not shaded, 1=shaded)'};
+    
+    OUTFILE.varsout(end+1,:) = {'subD'          ;'kg m^{-3}';'sample'   ;'Density'};
+    OUTFILE.varsout(end+1,:) = {'subT'          ;'K'        ;'sample'   ;'Temperature'};
+    OUTFILE.varsout(end+1,:) = {'subS'          ;'mm w.e.'  ;'sample'   ;'Slush water content'};
+    OUTFILE.varsout(end+1,:) = {'subW'          ;'mm w.e.'  ;'sample'   ;'Irreducible water'};
+    OUTFILE.varsout(end+1,:) = {'subZ'          ;'m'        ;'sample'   ;'Layer thickness'};
+    
+    io.varsout = struct('varname',{OUTFILE.varsout{1:end,1}},...
+        'units',{OUTFILE.varsout{1:end,2}},'type',{OUTFILE.varsout{1:end,3}},...
+        'description',{OUTFILE.varsout{1:end,4}});
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Update OUTFILE.TEMP with variables to be stored
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-fn = io.varsout;
-for i=1:numel(fn)
-    temp_long = OUT.(fn{i});
-    if t==1
-        OUTFILE.TEMP.(fn{i}) = zeros(size(temp_long));
+fn = OUTFILE.varsout;
+for i=1:length(fn)
+    temp_long = OUT.(fn{i,1});
+    if mod(t-1,io.freqout)==0
+        OUTFILE.TEMP.(fn{i,1}) = zeros(size(temp_long));
     end
-    if strcmp(io.typeout{i},'sample')
+    if strcmp(fn{i,3},'sample')
         if mod(t+floor(io.freqout/2),io.freqout)==0
-            OUTFILE.TEMP.(fn{i}) = temp_long';
+            OUTFILE.TEMP.(fn{i,1}) = temp_long';
         end
-    elseif strcmp(io.typeout{i},'mean')
-        OUTFILE.TEMP.(fn{i}) = OUTFILE.TEMP.(fn{i}) + temp_long/io.freqout;
-    elseif strcmp(io.typeout{i},'sum')
-        OUTFILE.TEMP.(fn{i}) = OUTFILE.TEMP.(fn{i}) + temp_long;
+    elseif strcmp(fn{i,3},'mean')
+        OUTFILE.TEMP.(fn{i,1}) = OUTFILE.TEMP.(fn{i,1}) + temp_long/io.freqout;
+    elseif strcmp(fn{i,3},'sum')
+        OUTFILE.TEMP.(fn{i,1}) = OUTFILE.TEMP.(fn{i,1}) + temp_long;
     end
+end
+if mod(t-1,io.freqout)==0
+    OUTFILE.output_time = time.TCUR/io.freqout;
+else
+    OUTFILE.output_time = OUTFILE.output_time + time.TCUR/io.freqout;
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -76,16 +81,16 @@ if t==1
        mkdir(io.outdir)
     end
     for i=1:length(fn)
-        io.fid(i) = fopen([io.outdir 'OUT_' fn{i} '.bin'], 'w');
+        io.fid(i) = fopen([io.outdir 'OUT_' fn{i,1} '.bin'], 'w');
     end
 end
 
 if mod(t,io.freqout)==0
     for i=1:length(fn)
-        OUTFILE.(fn{i}) = OUTFILE.TEMP.(fn{i});
-        fwrite(io.fid(i),OUTFILE.(fn{i}),'real*4');
-        OUTFILE.TEMP.(fn{i}) = 0.0;
+        OUTFILE.(fn{i,1}) = OUTFILE.TEMP.(fn{i,1});
+        fwrite(io.fid(i),OUTFILE.(fn{i,1}),'real*4');
     end
+    io.output_times(end+1,1:6) = datevec(OUTFILE.output_time);
 end
 
 if t==time.tn
@@ -95,16 +100,15 @@ if t==time.tn
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Save run info to file
+%%% Save run info to runinfo.mat in the output directory
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if t==time.tn
+    io.output_times = datetime(io.output_times);
     runinfo.grid = grid;
     runinfo.time = time;
     runinfo.IOout = io;
     runinfo.Cout = C;
-    cd(io.outdir);
-    save(io.infofile,'-struct','runinfo');
-    cd(io.homedir);
+    save([io.outdir 'runinfo.mat'],'-struct','runinfo');
 end
 
 end
