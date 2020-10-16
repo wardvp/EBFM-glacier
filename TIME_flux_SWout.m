a1e771ddfe544d6d2ef6fa834f8ade2334b79a48
wardvp <63638856+wardvp@users.noreply.github.com>
2020-10-16 11:02:28 +0200

    Updated use of snowmass in albedo calculation

--- TIME_flux_SWout.m
+++ TIME_flux_SWout.m
@@ -8,8 +8,8 @@ function [SWout,OUT] = TIME_flux_SWout(C,time,OUT,SWin)
 %%% Albedo [SOURCE: Oerlemans and Knap (1998), Bougamont et al. (2005), 
 %%%         Van Pelt et al. (2019)]
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ice_cond = OUT.subD(:,1)==C.Dice | OUT.snowmass==0;
-snow_cond = OUT.subD(:,1)<C.Dice & OUT.snowmass>0;
+ice_cond = OUT.subD(:,1)==C.Dice | OUT.snowmass(:,1)==0;
+snow_cond = ~ice_cond;
 
 % For a snow surface
 OUT.tstar(OUT.Tsurf==C.T0 & snow_cond) = C.tstar_wet;
@@ -21,7 +21,7 @@ OUT.alb_snow(snow_cond & OUT.timelastsnow<time.TCUR) = OUT.alb_snow( ...
     & OUT.timelastsnow<time.TCUR)*time.dt;
 OUT.alb_snow(OUT.timelastsnow==time.TCUR) = C.alb_fresh;
 OUT.alb_snow(ice_cond) = C.alb_ice;
-OUT.alb(snow_cond,1) = OUT.alb_snow(snow_cond,1);
+OUT.alb(snow_cond,1) = OUT.alb_snow(snow_cond);
 
 % For an ice surface
 OUT.alb(ice_cond,1) = C.alb_ice;
